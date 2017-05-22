42c59359a90089006676907314fa1474557bd518
cshivankar <cshivankar@gmail.com>
2017-05-22 08:28:41 +0530

    Update TestMain.java

--- demo/cart/TestMain.java
+++ demo/cart/TestMain.java
@@ -3,24 +3,9 @@ package com.demo.cart;
 public class TestMain {
 
 	public static void main(String[] args) {
-	/*	Items item1 = new Items("Banana", 2, 3);
-		Items item2 = new Items("Orange", 5, 3);
-		Items item3 = new Items("Apple", 10, 3);
-		Items item4 = new Items("Lemon", 1, 3);
-		Items item5 = new Items("Peach", 7, 3);
-		
-		
-		Basket basket = new Basket();
-		
-		basket.addItem(item1);
-		basket.addItem(item2);
-		basket.addItem(item3);
-		basket.addItem(item4);
-		basket.addItem(item5);
-		
-		basket.printTotalForAllItems();
-	*/
+	
 	Cart cart = new Cart();
 	cart.displayInputOptions();
+	
 	}
 }
